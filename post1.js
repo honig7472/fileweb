2d9090b67bdfb18f742fde4b0c1cd218619033fa
honig7472 <33463767+honig7472@users.noreply.github.com>
2019-01-26 22:12:36 +0700

    Add files via upload

---
+++ post1.js
@@ -0,0 +1,18 @@
+Langkah Install ( Wajib baca )
+
+    Uninstall IDM versi lama, pilih Full Uninstall
+    next Install IDM versi baru sampai selesai
+    kemudian buka folder Crack, Copy “IDMan.exe“, lalu paste ke
+
+C:\Program Files (x86)\Internet Download Manager
+
+    Replace yang asli
+    apabila tidak bisa di replace, cek seperti berikut
+
+Jika ada notif, The Action Can’t be completed, silahkan exit dulu Icon IDM nya di system Try
+
+
+    kemudian replace lagi lalu jalankan IDM nya
+    setelah itu doubleclick “KeyReg” nya, pilih yes
+    done full version
+    salam luar biasa
