d0bf5060a5614fd197b18f4869c085b697a60243
honig7472 <33463767+honig7472@users.noreply.github.com>
2019-02-01 01:17:40 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -19,18 +19,18 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*19)+ 1);
 var xxx=null;
-if(x=="1"){xxx="a-guide-to-personalizing-cell-phones.html"}
-if(x=="2"){xxx="work-at-home-based-business.html"}
-if(x=="3"){xxx="the-merits-of-using-cell-phones-for-business.html"}
-if(x=="4"){xxx="discover-how-to-make-money-from-home.html"}
-if(x=="5"){xxx="how-to-make-working-from-home-success.html"}
-if(x=="6"){xxx="how-to-choose-mobile-phone-wholesaler.html"}
-if(x=="7"){xxx="how-to-make-money-with-mobile-phones.html"}
-if(x=="8"){xxx="how-to-unlock-your-iphone.html"}
-if(x=="9"){xxx="auto-insurance-and-leasing.html"}
-if(x=="10"){xxx="how-to-calculate-your-lease-payment.html"}a_to_ve[i].href="https://securelink.electronicmagz.com/2019/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="01/a-guide-to-personalizing-cell-phones.html"}
+if(x=="2"){xxx="01/work-at-home-based-business.html"}
+if(x=="3"){xxx="01/the-merits-of-using-cell-phones-for-business.html"}
+if(x=="4"){xxx="01/discover-how-to-make-money-from-home.html"}
+if(x=="5"){xxx="01/how-to-make-working-from-home-success.html"}
+if(x=="6"){xxx="01/how-to-choose-mobile-phone-wholesaler.html"}
+if(x=="7"){xxx="01/how-to-make-money-with-mobile-phones.html"}
+if(x=="8"){xxx="01/how-to-unlock-your-iphone.html"}
+if(x=="9"){xxx="01/auto-insurance-and-leasing.html"}
+if(x=="10"){xxx="01/how-to-calculate-your-lease-payment.html"}a_to_ve[i].href="https://securelink.electronicmagz.com/2019/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
